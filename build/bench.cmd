2e29eda59191ff67b99036df56cd73ec2f42099b
YinzCN <Yinzcn@Gmail.com>
2011-09-26 15:58:41 +0800

    3

--- build/bench.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set n=10000
-set c=1000
-if not #%1==# set n=%1
-if not #%2==# set c=%2
-@echo on
-ab -n %n% -c %c% http://127.0.0.1:8080/%3
--- build/php-fcgi.cmd
+++
@@ -1,6 +0,0 @@
-
-set PHP_FCGI_MAX_REQUESTS=100000
-
-pushd php
-php-cgi -b 127.0.0.1:9900
-popd
