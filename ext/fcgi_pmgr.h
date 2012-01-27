c997b901f9a8b950db46f3078107c187bd46f07b
Yinzcn <YinzCN@Gmail.com>
2012-01-27 23:21:11 +0800

    fb

---
+++ ext/fcgi_pmgr.h
@@ -0,0 +1,41 @@
+
+/**
+  @author Ying Zeng (YinzCN_at_Gmail.com)
+**/
+
+
+#ifndef CHTD_FCGI_H
+#define CHTD_FCGI_H
+
+
+#include "chtd.h"
+#include "fastcgi.h"
+
+
+struct fcgi_proc_t
+{
+    int    port;
+    int    n_conn_max;
+    int    n_conn_curr;
+    struct usa_t rsa;
+    struct fcgi_proc_t *prev;
+    struct fcgi_proc_t *next;
+}
+
+
+/* Process Manager */
+struct fcgi_pmgr_t
+{
+    int    n_conn_max;
+    int    n_conn_curr;
+    char   extname[16];
+    char   fcgi_addr[64];
+    char   fcgi_port[16];
+    char   fcgi_cmdl[256];
+    pthread_mutex_t mutex;
+    struct usa_t rsa;
+    struct fcgi_proc_t *fcgi_pmgrs;
+    struct htdx_t *htdx;
+    struct fcgi_pmgr_t *prev;
+    struct fcgi_pmgr_t *next;
+};
